61fd67f393e40f9769815ce8195584c7e3a0354e
Nikidudu <nikisutedja@gmail.com>
2026-06-26 07:01:40 +0800

    fixed and reformated inital CPU

--- instructionset.v
+++ instructionset.v
@@ -4,36 +4,106 @@ module Memory(
     output [31:0] mem_rdata,
     input mem_rstrb // goes high when processor wants to read
     input [31:0] mem_wdata,
-    input [3:0] mem_wmask
+    input [3:0] mem_wmask // how many bytes to write, in a 4 byte word
 );
-reg [31:0] MEM [0:255];
+
+
+reg [31:0] MEM [0:1535]; //6KB RAM
+
+
+/// IDK WHAT THIS MEANS LOWKEY
+
+`ifdef BENCH
+   localparam slow_bit=12;
+`else
+   localparam slow_bit=17;
+`endif
+
+   // Memory-mapped IO in IO page, 1-hot addressing in word address.
+   localparam IO_LEDS_bit      = 0;  // W five leds
+   localparam IO_UART_DAT_bit  = 1;  // W data to send (8 bits)
+   localparam IO_UART_CNTL_bit = 2;  // R status. bit 9: busy sending
+
+   // Converts an IO_xxx_bit constant into an offset in IO page.
+   function [31:0] IO_BIT_TO_OFFSET;
+      input [31:0] bitid;
+      begin
+	 IO_BIT_TO_OFFSET = 1 << (bitid + 2);
+      end
+   endfunction
 
 `include "riscv_assembly.v"
-   integer L0_   = 4;
-   integer wait_ = 20;
-   integer L1_   = 28;
-   
+   integer    L0_      = 12;
+   integer    L1_      = 20;
+   integer    L2_      = 52;
+   integer    wait_    = 104;
+   integer    wait_L0_ = 112;
+   integer    putc_    = 124;
+   integer    putc_L0_ = 132;
+
    initial begin
+      LI(sp,32'h1800);   // End of RAM, 6kB
+      LI(gp,32'h400000); // IO page
+
+   Label(L0_);
+
+      // Count from 0 to 15 on the LEDs
+      LI(s0,16); // upper bound of loop
       LI(a0,0);
-   Label(L0_); 
+   Label(L1_);
+      SW(a0,gp,IO_BIT_TO_OFFSET(IO_LEDS_bit));
+      CALL(LabelRef(wait_));
       ADDI(a0,a0,1);
-      CALL(LabelRef(wait_)); 
-      J(LabelRef(L0_)); 
-      
-      EBREAK();
+      BNE(a0,s0,LabelRef(L1_));
+
+      // Send abcdef...xyz to the UART
+      LI(s0,26); // upper bound of loop
+      LI(a0,"a");
+      LI(s1,0);
+   Label(L2_);
+      CALL(LabelRef(putc_));
+      ADDI(a0,a0,1);
+      ADDI(s1,s1,1);
+      BNE(s1,s0,LabelRef(L2_));
+
+      // CR;LF
+      LI(a0,13);
+      CALL(LabelRef(putc_));
+      LI(a0,10);
+      CALL(LabelRef(putc_));
+
+      J(LabelRef(L0_));
+
+      EBREAK(); // I systematically keep it before functions
+                // in case I decide to remove the loop...
 
    Label(wait_);
-      LI(a1,1);
-      SLLI(a1,a1,slow_bit);
-   Label(L1_);
-      ADDI(a1,a1,-1);
-      BNEZ(a1,LabelRef(L1_));
+      LI(t0,1);
+      SLLI(t0,t0,slow_bit);
+   Label(wait_L0_);
+      ADDI(t0,t0,-1);
+      BNEZ(t0,LabelRef(wait_L0_));
       RET();
-	  
+
+   Label(putc_);
+      // Send character to UART
+      SW(a0,gp,IO_BIT_TO_OFFSET(IO_UART_DAT_bit));
+      // Read UART status, and loop until bit 9 (busy sending)
+      // is zero.
+      LI(t0,1<<9);
+   Label(putc_L0_);
+      LW(t1,gp,IO_BIT_TO_OFFSET(IO_UART_CNTL_bit));
+      AND(t1,t1,t0);
+      BNEZ(t1,LabelRef(putc_L0_));
+      RET();
+
       endASM();
    end
 
+/// IDK WHAT THIS MEANS LOWKEY
+
 wire [29:0] word_addr = mem_addr[31:2];
+
 always @(posedge clk) begin
     if (mem_rstrb) begin
         mem_rdata <= MEM[mem_addr[31:2]]; //31:2 for byte adress
@@ -54,9 +124,9 @@ module Processor(
     output mem_rstrb,
     output [31:0] mem_wdata,
     output [3:0] mem_wmask,
-    output reg [31:0] x10 = 0
-)
+);
 
+reg [31:0] PC = 0;
 reg [31:0] instr;   // current instructions
 // instructions grouped, all of these reperesnt the 7:0 opcode
 
@@ -71,16 +141,6 @@ wire isSYSTEM = {instr[6:0] == 7'1110011};
 wire isJAL = {instr[6:0] == 7'1101111}; // rd <- PC+4; PC<-PC+Jimm
 wire isJALR =  (instr[6:0] == 7'b1100111); // rd <- PC+4; PC<-rs1+Iimm
 
-// getting rs1, rs2, rd
-
-wire [4:0] rs1Id = instr[19:15];
-wire [4:0] rs2Id = instr[24:20];
-wire [4:0] rdIdId  = instr[11:7];
-
-// function codes
-wire [2:0] funct3 = instr[14:12];
-wire [6:0] funct7 = instr[31:25];
-
 
 // types
 wire [31:0] Iimm={{21{instr[31]}}, instr[30:20]};
@@ -90,17 +150,44 @@ wire [31:0] Uimm={instr[31:12], {12{1'b0}}};
 wire [31:0] Uimm={{12{instr[31]}}, instr[19:12], instr[20], instr[30:21],1'b0};
 
 
+// getting rs1, rs2, rd
+wire [4:0] rs1Id = instr[19:15];
+wire [4:0] rs2Id = instr[24:20];
+wire [4:0] rdIdId  = instr[11:7];
+
+// function codes
+wire [2:0] funct3 = instr[14:12];
+wire [6:0] funct7 = instr[31:25];
+
+// The registers bank
+   reg [31:0] RegisterBank [0:31];
+   reg [31:0] rs1; // value of source
+   reg [31:0] rs2; //  registers.
+   wire [31:0] writeBackData; // data to be written to rd
+   wire writeBackEn;   // asserted if data should be written to rd
+
+`ifdef BENCH
+   integer     i;
+   initial begin
+      for(i=0; i<32; ++i) begin
+	 RegisterBank[i] = 0;
+      end
+   end
+`endif
+
 // The ALU
 
 wire aluIn1 = rs1;
 wire aluIn2 = isAlureg|isBranch ? rs2 : Iimm;
-reg [31:0] aluOut;
-reg [4:0] shamt = isALureg ? rs2[4:0] : instr[24:20];
+
+reg [4:0] shamt = isALureg ? rs2[4:0] : instr[24:20]; // shift amount
+
+
+
 
 // optimization
-
-wire [32:0] aluMinus =  {1'b0,aluIn1} + {1'b1, ~aluIn2}+ 33'b1; 
 wire [31:0] aluPlus = aluIn1 + aluIn2;
+wire [32:0] aluMinus =  {1'b0,aluIn1} + {1'b1, ~aluIn2}+ 33'b1; 
 
 wire EQ = (aluMinus == 0);
 wire LTU = (aluMinus[32]);
@@ -123,6 +210,8 @@ wire [31:0] shifter =
 wire [31:0] leftshift = flip32(shifter);
 
 
+// ADD/SUB/ADDI
+reg [31:0] aluOut;
 always @(*) begin
     case (funct3)
         3'b000: aluOut = (funct7[5] & instr[5]) ? aluMinus[31:0] : aluPlus;
@@ -152,26 +241,28 @@ always @(*) begin
 end
 
 
-// decoding
-
-
-
-wire [31:0] writeBackData = (isJAL || isJALR) ? PCplus4 :
-			                isLUI         ? Uimm :
-			                isAUIPC       ? PCplusImm : 
-			                aluOut;
-wire writeBackEn = (state == EXECUTE && !isBranch && !isStore && !isLoad) || (state == WAIT_DATA);
+// Next PC computation
+// branch->PC+Bimm    AUIPC->PC+Uimm    JAL->PC+Jimm
 
 wire [31:0] PCplusImm = PC + ( instr[3] ? Jimm[31:0] :
 				        instr[4] ? Uimm[31:0] :
 				        Bimm[31:0] );
 
-
-
 wire [31:0] PCplus4 = PC+4;
-wire [31:0] nextPC =    ((isBranch && takeBranch) || isJAL) ? PCplusImm  :	       
-	                    isJALR                              ? {aluPlus[31:1],1'b0}:
-	                    PCplus4;
+
+// register writeback
+assign [31:0] writeBackData = (isJAL || isJALR) ? PCplus4 :
+			                isLUI         ? Uimm :
+			                isAUIPC       ? PCplusImm : 
+                            isLoad          ? LOAD_data :
+			                aluOut;
+
+wire [31:0] nextPC = ((isBranch && takeBranch) || isJAL) ? PCplusImm   :
+	                                  isJALR   ? {aluPlus[31:1],1'b0} :
+	                                             PCplus4;
+
+
+
 
 
 
@@ -181,11 +272,9 @@ wire [31:0] loadstore_addr = rs1 + (isStore ? Simm : Iimm);
 wire mem_byteAccess     = funct3[1:0] == 2'b00;
 wire mem_halfwordAccess = funct3[1:0] == 2'b01; 
 
-wire [15:0] LOAD_halfword =
-        loadstore_addr[1] ? mem_rdata[31:16] : mem_rdata[15:0];
+wire [15:0] LOAD_halfword = loadstore_addr[1] ? mem_rdata[31:16] : mem_rdata[15:0];
 
-wire  [7:0] LOAD_byte =
-        loadstore_addr[0] ? LOAD_halfword[15:8] : LOAD_halfword[7:0];
+wire  [7:0] LOAD_byte = loadstore_addr[0] ? LOAD_halfword[15:8] : LOAD_halfword[7:0];
 
 wire LOAD_sign = !funct3[2] & (mem_byteAccess ? LOAD_byte[7] : LOAD_halfword[15]);
 
@@ -213,7 +302,6 @@ wire [3:0] STORE_wmask =
             4'b1111;    
 
 
-reg [31:0] RegisterBank [0:31];
 localparam FETCH_INSTR=0;
 localparam WAIT_INSTR=1;
 localparam FETCH_REGS=2;
@@ -228,8 +316,8 @@ reg [2:0] state = FETCH_INSTR;
 always @(posedge clk) begin
 
     if(!resetn) begin
-	 PC    <= 0;
-	 state <= FETCH_INSTR;
+        PC    <= 0;
+        state <= FETCH_INSTR;
     end else begin
 
     //write back
@@ -260,6 +348,10 @@ always @(posedge clk) begin
             state <=    isLoad ? LOAD : 
                         isStore ? STORE:
                         FETCH_INSTR;
+
+            `ifdef BENCH
+	            if(isSYSTEM) $finish();
+            `endif  
         end
         LOAD: begin
 	        state <= WAIT_DATA;
@@ -274,8 +366,9 @@ always @(posedge clk) begin
     end 
 end
 
-assign mem_addr = (state == WAIT_INSTR || state == FETCH_INSTR) ?
-		     PC : loadstore_addr ;
+wire writeBackEn = (state == EXECUTE && !isBranch && !isStore) || (state == WAIT_DATA);
+
+assign mem_addr = (state == WAIT_INSTR || state == FETCH_INSTR) ? PC : loadstore_addr ;
 assign mem_rstrb = (state == FETCH_INSTR || state == LOAD);
 assign mem_wmask = {4{(state == STORE)}} & STORE_wmask;
 
